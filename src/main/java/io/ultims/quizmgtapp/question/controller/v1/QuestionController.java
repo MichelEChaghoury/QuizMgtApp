bd423e589aabdb60c96b698a70b3e4a6c8664e03
MichelChaghoury <michel.e.chaghoury@gmail.com>
2023-06-22 21:27:42 +0300

    Initial Commit.

--- src/main/java/io/ultims/quizmgtapp/question/controller/v1/QuestionController.java
+++ src/main/java/io/ultims/quizmgtapp/question/controller/v1/QuestionController.java
@@ -1,5 +1,10 @@
 package io.ultims.quizmgtapp.question.controller.v1;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuestionController {
-    
+
 }
